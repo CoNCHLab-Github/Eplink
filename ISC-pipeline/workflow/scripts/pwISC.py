0f793da4c2c6b207aff555822b2583a85e4d8f5d
Ali <atafakko@uwo.ca>
2024-04-04 16:48:29 -0400

    pairwise ISC script implemented, TODO: determine the number of time points included in the analysis automatically.

--- ISC-pipeline/workflow/scripts/pwISC.py
+++ ISC-pipeline/workflow/scripts/pwISC.py
@@ -1,40 +1,110 @@
 import nibabel as nib
 import numpy as np
+import pandas as pd
+import re
 import h5py
+import os
 
-# f = open(snakemake.output.h5, "a")
-# f.write(type(snakemake.input.func))
-# f.close()
+# Helper functions
+def get_dimensions(filepath):
+    """Extract dimensions of data from h5 data."""
+    # Check file extension
+    ext = os.path.splitext(filepath)[-1]
+    
+    # h5 file
+    if ext == '.h5': 
+        # Open the HDF5 file
+        with h5py.File(filepath, 'r') as f:
+            # Load the parcellated data
+            data = f['parcellated_data'][:]
+            # Return data dimensions
+            n_rois, n_vol = data.shape
+    
+    # gii file
+    elif ext == '.gii':
+        # Load the GIFTI file by nibabel
+        gii = nib.load(filepath)
+        # Return data dimensions
+        n_rois = gii.darrays[0].data.shape
+        n_vol = len(gii.darrays)
+    
+    return n_rois, n_vol
+
+def load_HDF(filepath, n_vols):
+    """Load data stored in the HDF files given the runs dataframe."""
+    with h5py.File(filepath, 'r') as f:
+            # Load the parcellated data
+            data = f['parcellated_data'][:]
+            data = data[:,:n_vols] # Ignoring excessive volumes
+    # Returns data with shape ROI x Time
+    return data
+
+def load_gii(filepath, n_vols):
+    func_gii = nib.load(filepath)
+    data = np.vstack([darray.data for darray in func_gii.darrays[:n_vols]]).T
+    # Returns data with shape Vertex x Time
+    return data
+
+def load_runs(runs_df, n_vols):
+    # Loading only the last run
+    fp = runs_df['Path'].iloc[-1]
+    # Checking file extension
+    ext = os.path.splitext(fp)[-1]
+
+    # h5 file
+    if ext == '.h5':
+        data = load_HDF(fp, n_vols)
+    # gii file
+    elif ext == '.gii':
+        data = load_gii(fp, n_vols)
+    
+    return data
+
+# Infer task form the output name
+pattern = re.compile(r"task-([^_]+)")
+match = pattern.search(snakemake.output.h5)
+target_task, = match.groups()
+
+# Gather info about the inputs and build a dataframe
+pattern = re.compile(r"sub-(\d+)/.*task-([^_]+)_run-(\d+)")
+
+inputs = []
 for f in snakemake.input.func:
-    print(f)
-    #f is string
+    match = pattern.search(f)
+    if match:
+        subject, task, run = match.groups()
+        if task == target_task: # filter for the task of interest
+            n_unit, n_vol = get_dimensions(f)
+            inputs.append({"Subject": subject, "Task": task, "Run": run, "#ROIs": n_unit, "#Vols": n_vol, "Path": f})
 
-# # Load functional data and atals file
-# func_gii = nib.load(snakemake.input.func)
-# label_gii = nib.load(snakemake.input.atlas)
+# Build a dataframe of input files
+df = pd.DataFrame(inputs)
+# Sort files 
+df = df.sort_values(by=['Subject', 'Task', 'Run'], ascending=[True]*3).reset_index()
+# Target volumes ===> TODO: a method to automatically determining the number of volumes
+target_volumes = 384
+# Exclude files with less volumes
+df = df[df["#Vols"] >= target_volumes].reset_index(drop=True)
+# Get unique subjects
+subjects = df['Subject'].unique()
 
-# # Extract the data arrays from the loaded GIfTI files
-# func_data = np.vstack([darray.data for darray in func_gii.darrays])
-# label_data = label_gii.darrays[0].data
-# label_table = label_gii.labeltable.get_labels_as_dict()
+# Load files data
+data = []
+for subj in subjects:
+    # Filter subject files 
+    df_s = df[df['Subject'] == subj]
+    # Load runs
+    data.append(load_runs(df_s, target_volumes))
 
-# # Get parcels from the label table
-# ROIs = label_table.keys()
+# Stacking subjects data (Subject, Unit, Time)
+data = np.stack(data)
+# Reorganizing data to (Unit, Subject, Time) shape for more efficient slicing when calculating ISCs
+data = np.transpose(data, axes=(1, 0, 2))
 
-# # Initialize a list to hold the parcellated functional data
-# parcellated_data = np.zeros((len(ROIs),func_data.shape[1]))
+pw_ISC = np.zeros((n_unit, len(subjects), len(subjects)))
+for i in range(n_unit):
+    pw_ISC[i,:,:] = np.corrcoef(data[i,:,:])
 
-# # Parcellate the functional data
-# for roi in ROIs:
-#     # Find vertices that belong to the current parcel
-#     vertices_in_parcel = np.where(label_data == roi)[0]
-    
-#     # Extract the functional data for these vertices and average across vertices
-#     parcel_time_series = np.mean(func_data[vertices_in_parcel, :], axis=0)
-    
-#     # Append the averaged time series to the parcellated data list
-#     parcellated_data[roi,:] = parcel_time_series
-
-# # Save Parcelated data as HDF5 file
-# with h5py.File(snakemake.output.h5, 'w') as f:
-#     f.create_dataset('parcellated_data', data=parcellated_data)
+# Save ISCs as HDF5 file
+with h5py.File(snakemake.output.h5, 'w') as f:
+    f.create_dataset('pw_ISC', data=pw_ISC)
