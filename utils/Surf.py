87ebe55f951e4f8e8f839cbdfb5b854f465628c9
Ali <alitafakkor@gmail.com>
2024-08-20 10:22:28 -0400

    updated surf util function to accept non dictionary input, fixed a bug with roi labels in some atlases

--- utils/Surf.py
+++ utils/Surf.py
@@ -4,19 +4,26 @@ import os
 
 def roi2gii(roi_data, atlasfile, fpath, fname, mask=None):
     """Create GIFTI Image from ROI values and save it in the results."""
+    # If data is not formatted as a dictionary we assume the first half corresponds to the left hemisphere
+    if ~isinstance(roi_data,dict):  
+        n_units = roi_data.shape[0]
+        roi_data = {'L': roi_data[:n_units//2], 'R': roi_data[n_units//2:]}
+
     # Iterate on Left and Right hemispheres
     surface_values = dict()
     for hemi in ['L', 'R']:
         # Load atlas file for the hemisphere
         label_gii = nib.load(atlasfile.format(hemi=hemi))
         label_data = label_gii.darrays[0].data
+
+        idx2label = np.sort(np.unique(label_data))
         # Copy ROI value to vertices 
         n_vertices = len(label_data)
         surface_values[hemi] = np.zeros(n_vertices, dtype='float32')
-        for roi_label, value in enumerate(roi_data[hemi]):
-            if (mask != None) and (not mask[hemi][roi_label]):
+        for roi_idx, value in enumerate(roi_data[hemi]):
+            if (mask != None) and (not mask[hemi][roi_idx]):
                 continue
-            vertices = (label_data == roi_label)
+            vertices = (label_data == idx2label[roi_idx])
             surface_values[hemi][vertices] = value
         
     vertex2gii(surface_values, fpath, fname)
